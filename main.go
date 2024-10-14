5cc9a09f48df09f87fbea106ca2886cd78daf423
cooler-SAI <cooler19860212@yahoo.com>
2024-10-14 21:17:51 +0300

    added improved code

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
+
+type Event struct {
+	Type string `json:"type"`
+	Repo struct {
+		Name string `json:"name"`
+	} `json:"repo"`
+}
+
+func fetchUserActivity(username string) ([]Event, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", username)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Error closing response body:", err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: failed to fetch data, status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var events []Event
+	err = json.Unmarshal(body, &events)
+	if err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -13,4 +55,14 @@ func main() {
 
 	username := os.Args[1]
 	fmt.Printf("Fetching activity for user: %s\n", username)
+
+	events, err := fetchUserActivity(username)
+	if err != nil {
+		fmt.Println("Error fetching activity:", err)
+		return
+	}
+
+	for _, event := range events {
+		fmt.Printf("%s event at %s\n", event.Type, event.Repo.Name)
+	}
 }
